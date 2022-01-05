177127a1d58ca16a8a1199021114349810a72a20
Marcus Wentz <52706599+MarcusWentz@users.noreply.github.com>
2022-01-04 23:41:32 -0500

    Update TruckStopTokenICO.sol

--- contracts/TruckStopTokenICO.sol
+++ contracts/TruckStopTokenICO.sol
@@ -9,12 +9,9 @@ contract TruckStopToken is ERC20{
     address immutable StrongManPersonalFinance = 0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2; //Need Metamask wallet address
     address immutable J_Naugy = 0x4B20993Bc481177ec7E8f571ceCaE8A9e22C02db; //Need Metamask wallet address from
 
-    address public TruckStopTokenAddress = address(this);    
-    TruckStopToken tokenObject = TruckStopToken(TruckStopTokenAddress);
-
     constructor() ERC20("TruckStopToken","TST") { //Fair ICOs like ETH have 80% public sale [We have 82%]. 1 quadrillion (10**15) tokens.
-        Owner = msg.sender;
-        _mint(Owner,                        (88)*(10**31) ); //Mint costs less gas than transfer. Owner will keep 6 but put 82 on Uniswap
+        Owner = msg.sender;                                  //Contract deployer is the owner.
+        _mint(Owner,                        (88)*(10**31) ); //Owner will keep 6 but put 82 on Uniswap.
         _mint(StrongManPersonalFinance,      (6)*(10**31) ); //Mint costs less gas than transfer. 
         _mint(J_Naugy,                       (6)*(10**31) ); //18 [base] + 13 [10 trillion] = 10**31
     }
